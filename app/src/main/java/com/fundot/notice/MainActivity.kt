45401d2a2148cfa92567b346b4d75cadacefd2be
hjqiaho <hjqiaho@163.com>
2023-06-16 17:16:47 +0800

    隐藏导航栏方法

--- app/src/main/java/com/fundot/notice/MainActivity.kt
+++ app/src/main/java/com/fundot/notice/MainActivity.kt
@@ -3,11 +3,14 @@ package com.fundot.notice
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
+import android.view.WindowManager
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import com.fundot.launcher.FundotLauncherHelper
+import com.gyf.immersionbar.BarHide
+import com.gyf.immersionbar.ImmersionBar
 
-class MainActivity :Activity(){
+class MainActivity : AppCompatActivity(){
 
     private var loginCallback: FundotLauncherHelper.FundotLoginStateCallback =
         object : FundotLauncherHelper.FundotLoginStateCallback{
@@ -27,7 +30,9 @@ class MainActivity :Activity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        ImmersionBar.with(this)
+            .hideBar(BarHide.FLAG_HIDE_NAVIGATION_BAR).init()
+//        window.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//A
         //监听 管控退出登录广播
         FundotLauncherHelper.addFundotLoginStateCallback(loginCallback)
         FundotLauncherHelper.addFundotTopAppChangeCallback(topAppChangeCallback)
@@ -37,6 +42,12 @@ class MainActivity :Activity(){
         findViewById<Button>(R.id.btn_login)?.setOnClickListener {
             val intent = Intent(this,LoginActivity::class.java)
             startActivity(intent)
+        }
+        findViewById<Button>(R.id.btn_hidden_nav)?.setOnClickListener {
+          FundotLauncherHelper.sendKeepAppBoardCast(this,"")
+        }
+        findViewById<Button>(R.id.btn_show_nav)?.setOnClickListener {
+            FundotLauncherHelper.sendRemoveKeepAppBoardCast(this)
         }
     }
 
